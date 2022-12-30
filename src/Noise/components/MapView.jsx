f851d62d91379b0f08e25c9e513c95d38eb6f430
Jennifer Intriago <jenniferintriago28@outlook.es>
2022-12-30 14:53:40 -0500

    Cambiar estilos de marcadores

--- src/Noise/components/MapView.jsx
+++ src/Noise/components/MapView.jsx
@@ -5,6 +5,8 @@ import { Loading } from "./Loading";
 import { Map, Marker, Popup } from "mapbox-gl";
 import { setMapa } from "../../store/map/mapSlice";
 import { Typography } from "@mui/material";
+import { AddLocationAltOutlined } from "@mui/icons-material";
+import { PopupMap } from "./PopupMap";
 
 export const MapView = () => {
   const { isLoading, userLocation, isMapReady, mapa, sensors } = useSelector(
@@ -42,18 +44,32 @@ export const MapView = () => {
         const market = [longitude, latitude];
         const myLocationPopup = new Popup().setHTML(
           `
-        <h3>Sensor ${sensor.sensor}</h3>
+        <div style='background-color: #15AABF; padding: 5px; color: #fff '>
+          <h3>Sensor ${sensor.sensor}</h3>
+        </div>
         <p><strong>Sensor</strong>: ${sensor.nombre}</p>
         <p><strong>Descripcion:</strong> ${sensor.description}</p>
-        <p><strong>dB:</strong> 75dB (umbral de dolor)</p>
+        <p><strong>Lugar:</strong> Sensor ubicado en ${userLocation}</p>
+
         <div style='display: flex; justify-content: center;'>
-          <img style='width: 100px'; object-fit: cover; margin: 0 auto:' src=${"https://cdn-icons-png.flaticon.com/512/853/853483.png?w=740&t=st=1671568416~exp=1671569016~hmac=dc7cb683e4ce77f84b41e4284fafb7cf96b71222c160dc89fad5e4e9f215990c"} />
+        <div style='width: 100px; height: 100px; background-color: #a29bfe; border-radius: 50%; display: flex; align-items: center;'>
+        <div style='padding: 10px'>
+          <p style='text-align: center;'><strong>dB:</strong> 75dB (umbral de dolor)</p>
+        </div>
+        </div>
+        </div>
+        
+       
+        <div style='display: flex; justify-content: center;  margin-top: 10px;'>
+          <img style='width: 80px'; object-fit: cover; margin: 0 auto:' src=${"https://cdn-icons-png.flaticon.com/512/853/853483.png?w=740&t=st=1671568416~exp=1671569016~hmac=dc7cb683e4ce77f84b41e4284fafb7cf96b71222c160dc89fad5e4e9f215990c"} />
         </div>
 
         `
         );
         const newMarket = new Marker({
           color: "#29AFC3",
+          // draggable: true,
+          // element: "<AddLocationAltOutlined />",
         })
           .setLngLat(market)
           .setPopup(myLocationPopup)
--- src/Noise/components/PopupMap.jsx
+++ src/Noise/components/PopupMap.jsx
@@ -1,6 +1,22 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import React from "react";
 
-export const PopupMap = () => {
-  return <Typography component="h3">Sensor: {sensor.sensor}</Typography>;
+export const PopupMap = ({ sensor }) => {
+  return (
+    <>
+      <Typography variant="h3">Sensor: {sensor.sensor}</Typography>
+      <Typography variant="p">Sensor: {sensor.nombre}</Typography>
+      <Typography variant="p">Descripción: {sensor.description}</Typography>
+      <Box
+        sx={{
+          width: 300,
+          height: 300,
+          backgroundColor: "primary",
+          borderRadius: "90%",
+        }}
+      >
+        <Typography variant="p">dB: 75dB (umbral de dolor)</Typography>
+      </Box>
+    </>
+  );
 };
