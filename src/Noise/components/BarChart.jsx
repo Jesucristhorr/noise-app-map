bd0da3b89afe77273910695d1baafa4ff8e3addc
Jennifer Intriago <jenniferintriago28@outlook.es>
2023-01-05 17:07:48 -0500

    paquete para graficas react

---
+++ src/Noise/components/BarChart.jsx
@@ -0,0 +1,27 @@
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const BarChart = ({ chartData }) => {
+  return (
+    <>
+      <Bar data={chartData} />
+    </>
+  );
+};
--- src/Noise/components/Charts.jsx
+++ src/Noise/components/Charts.jsx
@@ -1,9 +1,65 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import { BarChart } from "./BarChart";
+import { useState } from "react";
+import { width } from "@mui/system";
+
+const dataset = [
+  {
+    id: 1,
+    year: 2016,
+    userGain: 80000,
+    userLost: 823,
+  },
+  {
+    id: 2,
+    year: 2017,
+    userGain: 45677,
+    userLost: 345,
+  },
+  {
+    id: 3,
+    year: 2018,
+    userGain: 78888,
+    userLost: 555,
+  },
+  {
+    id: 4,
+    year: 2019,
+    userGain: 90000,
+    userLost: 4555,
+  },
+  {
+    id: 5,
+    year: 2020,
+    userGain: 4300,
+    userLost: 234,
+  },
+];
 
 export const Charts = () => {
+  const [userData, setUserData] = useState({
+    labels: dataset.map((data) => data.year),
+    datasets: [
+      {
+        label: "Users Gained",
+        data: dataset.map((data) => data.userGain),
+        backgroundColor: ["#74b9ff", "#dfe6e9"],
+      },
+    ],
+  });
   return (
     <>
       <Typography variant="h6">Este apartado es para graficos</Typography>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          width: "800px",
+        }}
+      >
+        <BarChart chartData={userData} />
+      </Box>
     </>
   );
 };
